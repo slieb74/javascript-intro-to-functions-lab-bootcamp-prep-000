dbb79f32e2625f1c0fe6013fc45b4ca51768bd07
Sam <sammyliebman@gmail.com>
2018-04-16 20:22:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase === string) return "I can't hear you!"
-  //else if (string.toUpperCase === string) return "YES INDEED!"
-  //else if (string = "I love you, Grandma.") return "I love you, too."
+  if (string = "I love you, Grandma.") return "I love you, too."
+  else if (string.toLowerCase === string) return "I can't hear you!"
+  else if (string.toUpperCase === string) return "YES INDEED!"
+  
 }
