b169b49047ca017aaff1da394ed22718f0873a59
Sam <sammyliebman@gmail.com>
2018-04-16 20:18:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function whisper(string){
 function logShout(string){
   
 }
+
+function logWhisper(string){
+  
+}
+
+function sayHiToGrandma(string){
+  if (string.toLowerCase === string) return "I can't hear you!"
+  else if (string.toUpperCase === string) return "YES INDEED!"
+  else if (string = "I love you, Grandma.") return "I love you, too."
+}
